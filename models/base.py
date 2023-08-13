4767eedec6fa9e14f423dd9660d8311fed3b8ebc
MohammadHossein Koosheshi <mhkooshesh21@gmail.com>
2023-08-13 22:33:06 +0330

    Fix base model

--- models/base.py
+++ models/base.py
@@ -20,26 +20,23 @@ class BaseConvDeconv():
         
         cnct = layers.concatenate([encoder1, encoder2])
         
-        up1 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same', name='tconv1_1')(cnct)
-        conv1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='upconv1_1')(up1)
-        conv1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='upconv1_2')(conv1)
+        up1 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', name='tconv1_1')(cnct)
+        conv1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv1_1')(up1)
+        conv1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv1_2')(conv1)
 
-        up2 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', name='tconv2_1')(conv1)
-        conv2 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv2_1')(up2)
-        conv2 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv2_2')(conv2)
+        up2 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', name='tconv2_1')(conv1)
+        conv2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv2_1')(up2)
+        conv2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv2_2')(conv2)
 
-        up3 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', name='tconv3_1')(conv2)
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv3_1')(up3)
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv3_2')(conv3)
+        up3 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', name='tconv3_1')(conv2)
+        conv3 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv3_1')(up3)
+        conv3 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv3_2')(conv3)
 
-        up4 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', name='tconv4_1')(conv3)
-        conv4 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv4_1')(up4)
-        conv4 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv4_2')(conv4)
-
-        up5 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same', name='tconv5_1')(conv4)
-        conv5 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv5_1')(up5)
-        conv5 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv5_2')(conv5)
-        conv6 = Conv2D(4, (1, 1), activation='linear', padding='same', name='upconv5_3')(conv5)
+        up4 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same', name='tconv4_1')(conv3)
+        conv4 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv4_1')(up4)
+        conv4 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv4_2')(conv4)
+        conv6 = Conv2D(4, (1, 1), activation='linear', padding='same', name='upconv5_3')(conv4)
+        
 
         based_model = Model(inputs=[input1, input2], outputs=[conv6[:,:,:,0], conv6[:,:,:,1], conv6[:,:,:,2], conv6[:,:,:,3]])
 
