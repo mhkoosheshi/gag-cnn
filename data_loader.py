02c45d1afe56ee0b9e91fe8df65ed164c9fb4307
MohammadHossein Koosheshi <mhkooshesh21@gmail.com>
2023-08-13 14:32:29 +0330

    Add data loader

---
+++ data_loader.py
@@ -0,0 +1,208 @@
+import numpy as np
+from PIL import Image
+import os
+import math
+import cv2
+from sklearn.model_selection import train_test_split
+import albumentations as A
+import random
+from tensorflow.keras.utils import Sequence
+from path_lists import train_path_lists, test_path_lists, unison_shuffle
+import albumentations as A
+
+class DataGenerator(Sequence):
+  '''
+  provide your model with batches of inputs and outputs with keras.utils.sequence
+
+  two branches of RGB inputs for sided cameras
+  '''
+  def __init__(self,
+              RGBobj_paths,
+              RGBiso_paths,
+              grasp_paths,
+              batch_size=8,
+              shape=(224,224),
+              shuffle=True,
+              aug_p=0.7
+              ):
+
+    self.RGBobj_paths = RGBobj_paths
+    self.RGBiso_paths = RGBiso_paths
+    self.grasp_paths = grasp_paths
+    self.batch_size = batch_size
+    self.shape = shape
+    self.shuffle = shuffle
+    self.aug_p = aug_p
+    self.on_epoch_end()
+
+    self.color_transform = A.Compose([
+    # A.ChannelShuffle(p=0.5),
+    A.CLAHE(p=0),
+    # A.ColorJitter(p=1),
+    A.Emboss(p=0),
+    # A.Equalize(mode='cv', by_channels=True, p=1),
+    A.FancyPCA(p=0.7),
+    A.GaussNoise(p=0.5),
+    A.ISONoise(p=0.9, color_shift=(0.01, 0.05), intensity=(0.3, 0.6)),
+    A.MultiplicativeNoise(p=0.5),
+    A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=50, p=0.7),
+    # A.RGBShift(p=1),
+    # A.ToGray(p=1),
+    A.RandomBrightnessContrast(p=0.7),
+    A.RandomBrightness(p=0.5),
+    A.Solarize(threshold=50, p=0.7),
+    A.PixelDropout(drop_value=0, dropout_prob=0.02, p=0.8),
+    A.RandomGamma(p=0.3),
+    A.RandomShadow(p=0.5, shadow_roi=(0, 0.7, 1, 1), num_shadows_lower=1, num_shadows_upper=2, shadow_dimension=4),
+    A.RandomToneCurve(p=0.7),
+    # A.RandomSunFlare(src_color=(255, 255, 255), p=1),
+], p=aug_p)
+
+  def on_epoch_end(self):
+    if self.shuffle:
+      ind = np.random.permutation(len(self.RGBobj_paths)).astype(np.int64)
+      self.RGBobj_paths, self.RGBiso_paths,  self.grasp_paths = np.array(self.RGBobj_paths), np.array(self.RGBiso_paths), np.array(self.grasp_paths)
+      self.RGBobj_paths, self.RGBiso_paths, self.grasp_paths = self.RGBobj_paths[ind], self.RGBiso_paths[ind], self.grasp_paths[ind]
+      self.RGBobj_paths, self.RGBiso_paths, self.grasp_paths = list(self.RGBobj_paths), list(self.RGBiso_paths), list(self.grasp_paths)
+
+
+  def __len__(self):
+    return math.ceil(len(self.RGBobj_paths) / self.batch_size)
+
+
+  def __getitem__(self, idx):
+
+    batch_RGBobj = self.RGBobj_paths[idx * self.batch_size : (idx+1) * self.batch_size]
+    batch_RGBiso = self.RGBiso_paths[idx * self.batch_size : (idx+1) * self.batch_size]
+    batch_grasp = self.grasp_paths[idx * self.batch_size : (idx+1) * self.batch_size]
+
+    rgbobj = []
+    rgbiso = []
+    grsp = []
+
+    for i, (RGBobj_path, RGBiso_path, grasp_path) in enumerate(zip(batch_RGBobj, batch_RGBiso, batch_grasp)):
+      
+      # RGB1 data
+      img = cv2.cvtColor(cv2.imread(RGBobj_path), cv2.COLOR_BGR2RGB)
+      pimg = (Image.fromarray(img)).resize((self.shape[0], self.shape[1]))
+      img = np.asarray(pimg)
+      # img = np.float32(img)
+      img = img
+
+      if self.aug_p !=0:
+        rnd = random.randint(1,2)
+        rnd = rnd - 1
+        img = (rnd)*(255 - img) + (1-rnd)*img
+        a = int(100*(random.random()))
+        random.seed(a)
+        transformed = self.color_transform(image=img)['image']
+        img = transformed
+
+      if self.aug_p !=0:
+        rnd = random.randint(1,2)
+        rnd = rnd - 1
+        img = (rnd)*(255 - img) + (1-rnd)*img
+
+      # img = np.float32(img)
+      rgbobj.append(img)
+
+
+      # RGB2 data
+      img = cv2.cvtColor(cv2.imread(RGBiso_path), cv2.COLOR_BGR2RGB)
+      pimg = (Image.fromarray(img)).resize((self.shape[0], self.shape[1]))
+      img = np.asarray(pimg)
+      # img = np.float32(img)
+      img = img
+      
+      if self.aug_p !=0:
+        rnd = random.randint(1,2)
+        rnd = rnd - 1
+        img = (rnd)*(255 - img) + (1-rnd)*img
+        random.seed(a)
+        transformed = self.color_transform(image=img)['image']
+        img = transformed
+
+      if self.aug_p !=0:
+        rnd = random.randint(1,2)
+        rnd = rnd - 1
+        img = (rnd)*(255 - img) + (1-rnd)*img
+
+      # img = np.float32(img)
+      rgbiso.append(img)
+
+      # grasp data
+      with open(grasp_path,"r") as f:
+        s = f.read()
+      grasp = [float(s.split(",")[i]) for i in range(0,len(s.split(",")))]
+      # grasp[0] = (grasp[0]-155)/(355-155)
+      # grasp[1] = (grasp[1]-185)/(410-185)
+      grasp[0] = (((0.5*grasp[0])/512 - 0.125)/0.25)
+      grasp[1] = (((0.5*grasp[1])/512 -0.0442 - 0.125)/0.25)
+      grasp[2] = (grasp[2]-0.01)/(0.08-0.01)
+      grasp[4] = (grasp[4]-25)/(105-25)
+      if grasp[3]<0:
+        grasp[3] = 270 + grasp[3]
+      elif grasp[3]>0:
+        grasp[3] = -90 + grasp[3]
+      grasp[3]=grasp[3]/180
+      
+      grsp.append(grasp)
+      
+      # print(a)
+
+    rgbobj = (np.array(rgbobj))/255
+    rgbiso = (np.array(rgbiso))/255
+    grsp = np.array(grsp)
+
+    return [rgbobj, rgbiso], [grsp]
+
+def get_loader(batch_size=8,
+              mode='rgb',
+              shape=(224,224),
+              shuffle=True,
+              factor=0.15,
+              aug=False,
+              aug_p=0):
+    # currently only for rgb
+    RGBobj_paths, RGBiso_paths, grasp_paths = train_path_lists(mode=mode)
+    n = len(RGBobj_paths)
+    RGBobj_paths, RGBiso_paths, grasp_paths = np.array(RGBobj_paths), np.array(RGBiso_paths), np.array(grasp_paths), np.array(grasp_paths)
+    RGBobj_paths, RGBiso_paths, grasp_paths = unison_shuffle(a=RGBobj_paths, b=RGBiso_paths, c=grasp_paths)
+    RGBobj_paths, RGBiso_paths, grasp_paths = list(RGBobj_paths), list(RGBiso_paths), list(grasp_paths)
+    RGBobj_train, RGBobj_val = RGBobj_paths[int(n*factor):], RGBobj_paths[:int(n*factor)]
+    RGBiso_train, RGBiso_val = RGBiso_paths[int(n*factor):], RGBiso_paths[:int(n*factor)]
+    grasp_train, grasp_val = grasp_paths[int(n*factor):], grasp_paths[:int(n*factor)]
+
+    if aug:
+        RGBobj_train, RGBiso_train, grasp_train = 2*RGBobj_train, 2*RGBiso_train, 2*grasp_train
+
+    # RGB1_test, RGB2_test, RGB3_test, grasp_test = test_path_lists(mode=mode)
+
+    train_gen = DataGenerator(RGBobj_train,
+                                RGBiso_train, 
+                                grasp_train,
+                                batch_size=batch_size,
+                                shape=shape,
+                                shuffle=shuffle,
+                                aug_p=aug_p
+                                )
+    val_gen = DataGenerator(RGBobj_val,
+                            RGBiso_val, 
+                            grasp_val,
+                            batch_size=batch_size,
+                            shape=shape,
+                            shuffle=shuffle,
+                            aug_p=0
+                            )
+
+    # test_gen = DataGenerator(RGB1_test,
+    #                             RGB2_test, 
+    #                             RGB3_test,
+    #                             grasp_test,
+    #                             batch_size=batch_size,
+    #                             shape=shape,
+    #                             shuffle=False,
+    #                             aug_p=0
+    #                             )
+
+    return train_gen, val_gen
---
+++ path_lists.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import glob
+
+
+def train_path_lists(mode='rgb'):
+  '''
+  mode : 'rgb' or 'rgbd' or 'd'
+  '''
+  grasps = []
+  rgb_obj = []
+  d_obj = []
+  rgb_iso = []
+  d_iso = []
+  
+  for c, grasp_path in enumerate(glob.glob("/content/drive/MyDrive/AppGraD/grasp/grasp*.txt")):
+      grasps.append(grasp_path)
+  for im_path in glob.glob(f"/content/drive/MyDrive/gag-cnn/obj/rgb*.png"):
+      rgb_obj.append(im_path)
+  for im_path in glob.glob(f"/content/drive/MyDrive/AppGraD/vs10/rgb*.png"):
+      rgb_iso.append(im_path)
+  
+  
+  if 'd' in mode:
+    for d_path in glob.glob(f"/content/drive/MyDrive/gag-cnn/vs10/d*.png"):
+        d_iso.append(d_path)
+    for d_path in glob.glob(f"/content/drive/MyDrive/gag-cnn/obj/d*.png"):
+      d_obj.append(d_path)
+
+  if mode=='rgb':
+    return rgb_obj, rgb_iso, grasps
+
+def test_path_lists():
+  # fix pers and iso labeling
+    list_grasp = []
+    list_RGB1 = []
+    list_D1 = []
+    list_RGB2 = []
+    list_D2 = []
+    list_RGB3 = []
+    list_D3 = []
+
+    for im_path in glob.glob(f"/content/drive/MyDrive/AppGraD/test/vs1/rgb*.png"):
+        list_RGB1.append(im_path)
+
+    for c, grasp_path in enumerate(glob.glob("/content/drive/MyDrive/AppGraD/test/grasp/grasp*.txt")):
+        list_grasp.append(grasp_path)
+
+    for d_path in glob.glob(f"/content/drive/MyDrive/AppGraD/test/vs1/d*.png"):
+        list_D1.append(d_path)
+
+    list_RGB1 = sorted(list_RGB1)
+    list_grasp = sorted(list_grasp)
+    list_D1 = sorted(list_D1)
+
+    return list_RGB1, list_D1, list_grasp
+
+def unison_shuffle(a, b, c, d=None):
+  if d is None:
+    inx=np.random.permutation(a.shape[0])
+    return a[inx],b[inx],c[inx]
+  elif d is not None:
+    inx=np.random.permutation(a.shape[0])
+    return a[inx], b[inx], c[inx], d[inx]
