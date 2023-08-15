b6b9034e90c76536816dc88f7ba553ea3465d8b3
MohammadHossein Koosheshi <mhkooshesh21@gmail.com>
2023-08-15 12:43:29 +0330

    Add linear activation

--- models/BaseConvDeconv.py
+++ models/BaseConvDeconv.py
@@ -25,20 +25,20 @@ class BaseConvDeconv():
         cnct = layers.concatenate([encoder1, encoder2])
         
         up1 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', name='tconv1_1')(cnct)
-        conv1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv1_1')(up1)
-        conv1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='upconv1_2')(conv1)
+        conv1 = Conv2D(256, (3, 3), activation='linear', padding='same', name='upconv1_1')(up1)
+        conv1 = Conv2D(256, (3, 3), activation='linear', padding='same', name='upconv1_2')(conv1)
 
         up2 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', name='tconv2_1')(conv1)
-        conv2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv2_1')(up2)
-        conv2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='upconv2_2')(conv2)
+        conv2 = Conv2D(128, (3, 3), activation='linear', padding='same', name='upconv2_1')(up2)
+        conv2 = Conv2D(128, (3, 3), activation='linear', padding='same', name='upconv2_2')(conv2)
 
         up3 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', name='tconv3_1')(conv2)
-        conv3 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv3_1')(up3)
-        conv3 = Conv2D(64, (3, 3), activation='relu', padding='same', name='upconv3_2')(conv3)
+        conv3 = Conv2D(64, (3, 3), activation='linear', padding='same', name='upconv3_1')(up3)
+        conv3 = Conv2D(64, (3, 3), activation='linear', padding='same', name='upconv3_2')(conv3)
 
         up4 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same', name='tconv4_1')(conv3)
-        conv4 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv4_1')(up4)
-        conv4 = Conv2D(32, (3, 3), activation='relu', padding='same', name='upconv4_2')(conv4)
+        conv4 = Conv2D(32, (3, 3), activation='linear', padding='same', name='upconv4_1')(up4)
+        conv4 = Conv2D(32, (3, 3), activation='linear', padding='same', name='upconv4_2')(conv4)
         conv6 = Conv2D(4, (1, 1), activation='linear', padding='same', name='upconv5_3')(conv4)
         
 
@@ -50,12 +50,12 @@ def Convlayer(input, outdim, is_batchnorm, name):
     x = Conv2D(outdim, (3, 3), strides=(1, 1), padding="same", name=name+'_1')(input)
     if is_batchnorm:
         x =BatchNormalization(name=name + '_1_bn')(x)
-    x = Activation('relu',name=name + '_1_act')(x)
+    x = Activation('linear',name=name + '_1_act')(x)
 
     x = Conv2D(outdim, (3, 3), strides=(1, 1), padding="same", name=name+'_2')(x)
     if is_batchnorm:
         x = BatchNormalization(name=name + '_2_bn')(x)
-    x = Activation('relu', name=name + '_2_act')(x)
+    x = Activation('linear', name=name + '_2_act')(x)
     return x
 
 def encoder(inputs, name):
@@ -67,8 +67,8 @@ def encoder(inputs, name):
     pool3 = MaxPool2D(pool_size=(2,2))(conv3)
     conv4 = Convlayer(pool3, 256, is_batchnorm=True, name=name+'4')
     pool4 = MaxPool2D(pool_size=(2,2))(conv4)
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', name=name+'5')(pool4)
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', name=name+'6')(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='linear', padding='same', name=name+'5')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='linear', padding='same', name=name+'6')(conv5)
     outputs = conv5
 
     return outputs
