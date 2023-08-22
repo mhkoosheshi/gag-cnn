21b99a657880367b2f810fae96a88790136d5c80
MohammadHossein Koosheshi <mhkooshesh21@gmail.com>
2023-08-22 13:31:00 +0330

    Add focal and tversky loss

--- utils/losses.py
+++ utils/losses.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.losses import Loss
+import tensorflow.keras.backend as K
 
 
 def jaccard_loss(y_pred, y_true, axis=(0, 1, 2, 3), smooth=1e-5):
@@ -9,6 +10,20 @@ def jaccard_loss(y_pred, y_true, axis=(0, 1, 2, 3), smooth=1e-5):
     jaccard = 1 - (inse + smooth) / (l + r - inse + smooth)
     jaccard = tf.reduce_mean(jaccard)
     return jaccard
+
+def tversky(y_true, y_pred):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+    false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
+    alpha = 0.7
+    return (true_pos + 1) / (true_pos + alpha * false_neg + (1 - alpha) * false_pos + 1)
+
+def focal_tversky(y_true, y_pred):
+    pt_1 = tversky(y_true, y_pred)
+    gamma = 0.75
+    return K.pow((1 - pt_1), gamma)
 
 def get_loss(loss_name='jaccard_loss'):
 
@@ -29,3 +44,9 @@ def get_loss(loss_name='jaccard_loss'):
 
     elif loss_name=='mselog_loss':
         return tf.keras.losses.MeanSquaredLogarithmicError()
+
+    elif loss_name=='tversky_loss':
+        return tversky
+
+    elif loss_name=='focal_tversky_loss':
+        return focal_tversky
