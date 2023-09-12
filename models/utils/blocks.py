fbf073a979b0e7a1d677a9ddd11a5559a91e3d8a
MohammadHossein Koosheshi <mhkooshesh21@gmail.com>
2023-09-12 23:03:25 +0330

    Add res blocks

--- models/utils/blocks.py
+++ models/utils/blocks.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers import Conv2DTranspose, Conv2D, MaxPool2D, BatchNormalization, Dropout, SeparableConv2D, Activation, UpSampling2D
+from tensorflow.keras.layers import Add, Conv2DTranspose, Conv2D, MaxPool2D, BatchNormalization, Dropout, SeparableConv2D, Activation, UpSampling2D
 from tensorflow.keras.models import Model
 
 def Conv(input, filters, pool=False, activation="relu", bias=True, init='glorot_uniform', bn=True):
@@ -22,4 +22,12 @@ def SepConv(input, filters, pool=False, activation="relu", bias=True, init='glor
     x = Activation(activation)(x)
     if pool:
         x = MaxPool2D(pool_size=(2,2))(x)
+    return x
+
+def ResConv(input, filters, activation="relu", bias=True, init='glorot_uniform', bn=True):
+    x = Conv(input, filters, pool=False, activation=activation, bias=bias, init=init, bn=bn)
+    x = Conv(x, filters, pool=False, activation=activation, bias=bias, init=init, bn=bn)
+    x_skip = input
+    x = Add()([x, x_skip])
+    # x = Activation(activation)(x)
     return x
